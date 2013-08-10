a2b3c9462395606de27a56ca046392ced75dbe8e
Thomas Smith <tom.smith@imulus.com>
2013-08-10 11:12:49 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ping::Application.config.secret_key_base = '32ef7c149e59a10e7562ad925505bb45f632648b9b5dcfe83b8d879bb64240520b1836377c9d5ffa8a8b10cae304c6afdcc62670df071744db15e81b60d2f8c0'
