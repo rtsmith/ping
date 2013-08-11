3ef80f4b27f15f555ce49816a84560c88676e0ca
Thomas Smith <tom.smith@imulus.com>
2013-08-11 10:47:07 -0600

    Add Game test

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ActiveRecord::Base
   validates :player1, presence: true
+  validates :player2, presence: true
 end
--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -4,4 +4,15 @@ class GameTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "game has players and they are strings" do 
+    game = Game.new(player1: "Samson", player2: "Delilah")
+    assert game.player1.is_a? String
+    assert game.player2.is_a? String
+  end
+
+  test "game without players shouldn't save" do
+    game = Game.new
+    assert !game.save
+  end
 end
